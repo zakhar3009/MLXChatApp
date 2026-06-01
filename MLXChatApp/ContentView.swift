038eb69c86275ceaa6a6dbb9a83510f118d5f84c
zakharlit <zakhlit@gmail.com>
2026-06-01 10:26:42 +0300

    Initial Commit

---
+++ MLXChatApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MLXChatApp
+//
+//  Created by Zakhar Litvinchuk on 01.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MLXChatApp/MLXChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLXChatAppApp.swift
+//  MLXChatApp
+//
+//  Created by Zakhar Litvinchuk on 01.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct MLXChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
