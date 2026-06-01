e1cedf40ffd57618e6f7f549b9fbf767f2aeef1f
zakharlit <zakhlit@gmail.com>
2026-06-01 10:50:26 +0300

    Create interface for chat service

---
+++ Packages/LLMChatKit/Sources/LLMChatKit/ChatEngine.swift
@@ -0,0 +1,19 @@
+//
+//  ChatEngine.swift
+//  LLMChatKit
+//
+//  Created by Zakhar Litvinchuk on 01.06.2026.
+//
+
+import Foundation
+
+public typealias SessionID = UUID
+
+public protocol ChatEngine {
+  func loadModelIfNeeded() async throws
+  
+  func createSession() async throws -> SessionID
+  func deleteSession(with id: SessionID) async
+  
+  func sendMessage(_ message: String, in session: SessionID) async throws -> String
+}
